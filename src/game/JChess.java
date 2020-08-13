103be10667184897f1e44b2d953cacf465e40be2
annabel159 <69480412+annabel159@users.noreply.github.com>
2020-08-13 13:52:43 +0200

    Add files via upload

---
+++ src/game/JChess.java
@@ -0,0 +1,16 @@
+package game;
+
+import game.board.Board;
+import gui.Table;
+
+public class JChess {
+
+	public static void main(String[] args) {
+		Board board = Board.createStandardBoard();
+
+		System.out.println(board);
+
+		Table.get().show();
+	}
+
+}
